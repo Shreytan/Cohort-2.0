01917b6b8a8495689360b0c949f68f1fe7fb6146
Shreyansh Shukla <shreyanshshukla7@gmail.com>
2025-04-24 11:33:06 +0530

    custom hook  beginner

--- MyTryouts/week_9_hooks/hooks-practice/src/App.jsx
+++ MyTryouts/week_9_hooks/hooks-practice/src/App.jsx
@@ -1,39 +1,35 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from 'axios'
+
+function useTodos() {
+  const [todos, setTodos] = useState([])
+
+  useEffect(() => {
+    axios.get("https://sum-server.100xdevs.com/todos")
+      .then(res => {
+        setTodos(res.data.todos);
+      })
+  }, [])
+
+  return todos;
+}
 
 function App() {
-
-  const [render,setRender] = useState(true);
-
-  useEffect(()=>{
-    setTimeout(() => {
-      setRender(!render);
-    }, 5000);
-  },[render]);
+  const todos = useTodos();
 
   return (
     <>
-      {render ? <Mycomponent/> : <div></div>}
+      {todos.map(todo => <Track todo={todo} />)}
     </>
-  );
+  )
+}
 
-  function Mycomponent() {
-    useEffect(
-      () => {
-        console.log('Mycomponent mounted');
-        return () => {
-          console.log('unmonted now');
-        }
-      }
-      , []);
-      return(
-        <>
-        hello from component
-        </>
-      )
-  }
+function Track({ todo }) {
+  return <div>
+    {todo.title}
+    <br />
+    {todo.description}
+  </div>
 }
 
 export default App
